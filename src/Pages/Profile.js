ab33359d4508749ebc9722535a18cb939f1f4918
Emily LD <lagreouemily@gmail.com>
2022-03-04 12:42:02 +0100

    change url

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -13,8 +13,8 @@ const Profile = ({ name, token }) => {
   const [favorites, setFavorites] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  //const urlbase = "https://happy-cow-eld.herokuapp.com";
-  const urlbase = "http://localhost:4000";
+  const urlbase = "https://happy-cow-eld.herokuapp.com";
+  //const urlbase = "http://localhost:4000";
 
   //get array of favorite places per placeId
   useEffect(() => {
